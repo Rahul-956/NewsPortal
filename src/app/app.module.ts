9cfe3698e3ed21d8c7bcde7b8c90872291bd06f3
Rahul-956 <rawatrahul956@gmail.com>
2021-11-10 20:10:10 +0530

    comit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-import {HttpClientModule} from '@angular/common/http';
-import {newsapiService} from './services/newsapi.service'
+import { HttpClientModule } from '@angular/common/http';
+import { newsapiService } from './services/newsapi.service';
 import { TopheadlineComponent } from './topheadline/topheadline.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { ContactComponent } from './contact/contact.component';
+import { TechnewsComponent } from './topheadline/newspanel/technews/technews.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { LoginComponent } from './login/login.component';
+import { NewspanelComponent } from './newspanel/newspanel.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     TopheadlineComponent,
-    ContactComponent
+    TechnewsComponent,
+    AboutusComponent,
+    // RegistrationComponent,
+    LoginComponent,
+    NewspanelComponent
   ],
   imports: [
     BrowserModule,
@@ -25,4 +31,5 @@ import { ContactComponent } from './contact/contact.component';
   providers: [newsapiService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
